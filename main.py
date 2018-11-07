5b4eb01fe4266849571a66f95ce234ddc7d4ea33
Jack Dubbs <dubbs.14@osu.edu>
2018-11-07 00:06:28 -0500

    finished the data gathering implementation of the project. Random data collected for NN.

--- main.py
+++ main.py
@@ -1,3 +1,7 @@
 import snake_gather_data as snake
 
-snake.run()
+file_name = 'C:\\Users\\Jack Dubbs\\Desktop\\Kaggle\\Hackathon1\\data\\6000_iterations'
+# This is how we open and close a file (with the with keyword)
+with open(file_name, 'w') as output_file:
+    for _ in range(6000):
+        snake.run(output_file)
--- snake_gather_data.py
+++ snake_gather_data.py
@@ -6,224 +6,119 @@ import time
 import random
 
 
-class Gui():
+class Data():
     def __init__(self, grid_cols, grid_rows, grid):
         # Length and height (x,y) of the window. Each [row][col] in the grid is 20 pixels so we mult our grid sizes by 20
-        self.length = grid_cols * 20
-        self.height = grid_rows * 20
         self.grid_cols = grid_cols
         self.grid_rows = grid_rows
-        self.frames = 10000
+        self.iterations = 10000
         self.got_food_score_change = -100
         self.died_score_change = 100000
         # Reference to the grid it came from
         self.grid = grid
         self.grid_data = []
+        self.grid_data_string = ''
         # Scores
         self.score = 0
         self.score_data_dynamic = []
+        self.score_data_dynamic_string = ''
         self.score_final = 0
         self.high_score = 0
-        # Delay Timer so we don't go so fast!
-        self.delay_start = 0
-        # This is the seconds the game waits after death
-        self.pause_delay = self.delay_start * 5
-        self.delay = self.delay_start
-        # The display window
-        self.window = turtle.Screen()
-        self.set_up_screen()
-        # The snake head
-        self.head = turtle.Turtle()
         self.move_data = []
+        self.move_data_string = ''
         self.set_snake_head()
-        # The snake body (the tail)
-        self.segments = []
-        # The Food
-        self.food = turtle.Turtle()
-        self.set_food()
-        # The score Baord (Drawn using a PEN)
-        self.pen = turtle.Turtle()
-        self.set_up_scoreboard()
         self.recursive_count = 0
-
-    # Takes the indices of the 2d array and cover to coordinate points
-    def convert_grid_to_coord(self, snake_node):
-        x = (-(self.height/2)) + (snake_node.get_col() * 20)
-        y = (self.length/2) + (-(snake_node.get_row()) * 20)
-        return [x, y]
-
-    # Update the score display
-    def update_score_disp(self):
-        self.pen.clear()
-        self.pen.write("Score: {}  High Score: {}".format(self.score, self.high_score),
-                       align="center", font=("Courier", 20, "normal"))
-
-    # Hide and clear the tail so it isn't in the next round
-    def hide_and_clear_segments(self):
-        # Hide the segments
-        for segment in self.segments:
-            segment.goto(1000, 1000)
-            segment.hideturtle()
-        # Clear the segments list
-        self.segments.clear()
-
-    # Set Up the Screen
-    def set_up_screen(self):
-        self.window.title("Jack's Snake Game!")
-        self.window.bgcolor("white")
-        self.window.setup(width=self.length, height=self.height)
-        self.window.tracer(0)  # Turns off screen updates
+        self.xdirection = 0
+        self.ydirection = 0
 
     # Sets up the Snake head. Places it in the center of the grid.
     def set_snake_head(self):
-        self.head.speed(0)  # makes this as fast as possible
-        self.head.shape("square")
-        self.head.color("black")
-        self.head.penup()
-        x, y = self.convert_grid_to_coord(self.grid.head)
-        self.head.goto(x, y)
-        self.head.xdirection = 0
-        self.head.ydirection = 0
-
-    def add_segment(self):
-        # Add a segment
-        new_segment = turtle.Turtle()
-        new_segment.speed(0)
-        new_segment.shape("square")
-        new_segment.color("black")
-        new_segment.penup()
-        self.segments.append(new_segment)
-
-    # Sets up the Snake Food
-    def set_food(self):
-        self.food.speed(0)
-        self.food.shape("square")
-        self.food.color("red")
-        self.food.penup()
-        x, y = self.convert_grid_to_coord(self.grid.food_location)
-        self.food.goto(x, y)
-
-    # Pen (for score board)
-    def set_up_scoreboard(self):
-        self.pen.speed(0)
-        self.pen.shape("square")
-        self.pen.color("black")
-        self.pen.penup()
-        self.pen.hideturtle()
-        self.pen.goto(0, (self.height / 2) - (self.height / 6))
-        self.pen.write("Score: 0  High Score: 0", align="center", font=("Courier", 20, "normal"))
+        self.xdirection = 0
+        self.ydirection = 0
 
     # Move the snake, and at any point if snake dead, break
     def move_snake(self):
         self.random_direction()
         self.recursive_count = 0
-        row_move = self.head.xdirection
-        col_move = self.head.ydirection
+        row_move = self.xdirection
+        col_move = self.ydirection
         self.grid.snake_move(row_move, col_move)
         # We did not die.
         if not self.grid.isdead:
             # We ate food, so food went to a new location, we grew, and moved
             if self.grid.grew:
                 self.eat_and_grow()
-
-            self.update_head_and_tail()
         # We did die. Reset the location of the snake head. restart.
         else:
             self.die()
 
     # This is what happens when we ate food! Yummy!
     def eat_and_grow(self):
-        x, y = self.convert_grid_to_coord(self.grid.food_location)
-        self.food.goto(x, y)
-        self.add_segment()
         # Change the Score for getting an apple
         self.score += self.got_food_score_change
-        self.update_score_disp()
-
-    # This is what happens to move the snake around the board, head and body
-    def update_head_and_tail(self):
-        # New snake head location
-        x, y = self.convert_grid_to_coord(self.grid.head)
-        self.head.goto(x, y)
-        # New tail locations
-        i = 0
-        for body_node in self.grid.tail.get_queue():
-            x, y = self.convert_grid_to_coord(body_node)
-            self.segments[i].goto(x, y)
-            i += 1
 
     # This is what happens when a snake dies.
     def die(self):
         self.score += self.died_score_change
         self.grid.__init__(self.grid_rows, self.grid_cols)
-        time.sleep(self.pause_delay)
-        x, y = self.convert_grid_to_coord(self.grid.head)
-        self.head.goto(x, y)
-        x, y = self.convert_grid_to_coord(self.grid.food_location)
-        self.food.goto(x, y)
-        self.head.xdirection = 0
-        self.head.ydirection = 0
-        self.hide_and_clear_segments()
-        # self.reset_score_and_delay()
-        self.update_score_disp()
+        self.xdirection = 0
+        self.ydirection = 0
 
     # Change the direction of the snake to up
     def go_up(self):
         if self.recursive_count > 50:
             self.die()
-        elif self.head.xdirection == 1:
+        elif self.xdirection == 1:
             # Try Again
-            self.head.xdirection = 0
-            self.head.ydirection = 0
+            self.xdirection = 0
+            self.ydirection = 0
             self.random_direction()
         else:
-            self.head.xdirection = -1
-            self.head.ydirection = 0
+            self.xdirection = -1
+            self.ydirection = 0
 
     # Change the direction of the snake to down
     def go_down(self):
         if self.recursive_count > 50:
             self.die()
-        elif self.head.xdirection == -1:
+        elif self.xdirection == -1:
             # Try Again
-            self.head.xdirection = 0
-            self.head.ydirection = 0
+            self.xdirection = 0
+            self.ydirection = 0
             self.random_direction()
         else:
-            self.head.xdirection = 1
-            self.head.ydirection = 0
+            self.xdirection = 1
+            self.ydirection = 0
 
     # Change the direction of the snake to left
     def go_left(self):
         if self.recursive_count > 50:
             self.die()
-        elif self.head.ydirection == 1:
+        elif self.ydirection == 1:
             # Try Again
-            self.head.xdirection = 0
-            self.head.ydirection = 0
+            self.xdirection = 0
+            self.ydirection = 0
             self.random_direction()
         else:
-            self.head.xdirection = 0
-            self.head.ydirection = -1
+            self.xdirection = 0
+            self.ydirection = -1
 
     # Change the direction of the snake to right
     def go_right(self):
         if self.recursive_count > 50:
             self.die()
-        elif self.head.ydirection == -1:
+        elif self.ydirection == -1:
             # Try Again
-            self.head.xdirection = 0
-            self.head.ydirection = 0
+            self.xdirection = 0
+            self.ydirection = 0
             self.random_direction()
         else:
-            self.head.xdirection = 0
-            self.head.ydirection = 1
+            self.xdirection = 0
+            self.ydirection = 1
 
     def move_food_random(self):
         # Move the food to random spot
         self.grid.spawn_food()
-        x, y = self.convert_grid_to_coord(self.grid.food_location)
-        self.food.goto(x, y)
 
     def random_direction(self):
         rand_direction = random.randint(0, 3)
@@ -236,8 +131,8 @@ class Gui():
             self.go_left()
         elif rand_direction == 3:
             self.go_right()
-        row_move = self.head.xdirection
-        col_move = self.head.ydirection
+        row_move = self.xdirection
+        col_move = self.ydirection
 
         while not self.grid.snake_check_move(row_move, col_move):
             rand_direction = random.randint(0, 3)
@@ -249,8 +144,8 @@ class Gui():
                 self.go_left()
             elif rand_direction == 3:
                 self.go_right()
-            row_move = self.head.xdirection
-            col_move = self.head.ydirection
+            row_move = self.xdirection
+            col_move = self.ydirection
 
     def print_grid(self):
         index = 0
@@ -265,6 +160,12 @@ class Gui():
         for element in self.grid.grid:
             self.grid_data.extend(element)
 
+    # Collects the state of the grid and create a single list of the entire game.
+    def get_grid_data_string(self):
+        for element in self.grid.grid:
+            for value in element:
+                self.grid_data_string += str(value)
+
     # Collects the state of the grid and appends each row into the list. list of rows
     def get_grid_data_rows(self):
         for element in self.grid.grid:
@@ -278,44 +179,75 @@ class Gui():
     def get_score_data_dynamic(self):
         self.score_data_dynamic.append(self.score)
 
+    # Collects the state of the current score, puts every score update in a list
+    def get_score_data_dynamic_string(self):
+        self.score_data_dynamic_string += str(self.score)
+        self.score_data_dynamic_string += ' '
+
     # Collects the staet of the current move, puts every move into a list. pretty for petty human eyes
     def get_move_data_for_humans(self):
         self.move_data.append('(')
-        self.move_data.append(self.head.xdirection)
+        self.move_data.append(self.xdirection)
         self.move_data.append(',')
-        self.move_data.append(self.head.ydirection)
+        self.move_data.append(self.ydirection)
         self.move_data.append(')')
         self.move_data.append(' ')
 
     # Collects the staet of the current move, puts every move into a list. pretty for petty human eyes
+    def get_move_data_for_humans_string(self):
+        self.move_data_string += '('
+        self.move_data_string += str(self.xdirection)
+        self.move_data_string += ','
+        self.move_data_string += str(self.ydirection)
+        self.move_data_string += ')'
+        self.move_data_string += ' '
+
+    # Collects the staet of the current move, puts every move into a list.
     def get_move_data(self):
-        self.move_data.append(self.head.xdirection)
-        self.move_data.append(self.head.ydirection)
+        self.move_data.append(self.xdirection)
+        self.move_data.append(self.ydirection)
+
+    # Collects the staet of the current move, puts every move into a list.
+    def get_move_data_string(self):
+        self.move_data_string += str(self.xdirection)
+        self.move_data_string += str(self.ydirection)
+        self.move_data_string += ' '
 
 
-def run():
-    grid_rows = 30
-    grid_cols = 30
+def run(output_file):
+    # row and col size of our board
+    grid_rows = 5 * random.randint(1, 20)
+    grid_cols = 5 * random.randint(1, 20)
 
     # Create Grid, Initialize Snake head, Spawn Food
-    # ReLuuus
+    # ReLuuus - possible ai thing to look up and use
     g = Grid(grid_rows, grid_cols)
 
-    gui = Gui(grid_cols, grid_rows, g)
+    data = Data(grid_cols, grid_rows, g)
 
-    # Main Game Loop. Runs for a total number of frames
-    for _ in range(gui.frames):
-        gui.score += 1
-        gui.update_score_disp()
-        gui.window.update()
+    # Main Game Loop. Runs for a total number of iterations
+    for _ in range(data.iterations):
+        data.score += 1
         # move the snake through the grid.
-        gui.move_snake()
-        gui.get_move_data_for_humans()
-        gui.get_grid_data_grids()
-        gui.get_score_data_dynamic()
-        # Pause the execution of the program so it doesn't go lightning fast
-        time.sleep(gui.delay)
+        data.move_snake()
+        data.get_move_data_for_humans()
+        data.get_score_data_dynamic()
+        # data.get_move_data_for_humans_string()
+        # data.get_score_data_dynamic_string()
 
-    print(gui.grid_data)
-    print(gui.score_data_dynamic)
-    print(gui.move_data)
+    # output_file.write(data.move_data_string)
+    # output_file.write(data.score_data_dynamic_string)
+
+    import json
+    # start with the grid rows and cols to be displayed for each iteration
+    json.dump(grid_rows, output_file)
+    json.dump(',', output_file)
+    json.dump(grid_cols, output_file)
+    json.dump(' ', output_file)
+    # add in the move data for each iteration
+    json.dump(data.move_data, output_file)
+    json.dump(' ', output_file)
+    # add in the score data for each iteration
+    json.dump(data.score_data_dynamic, output_file)
+    # Seperate iterations with a ! character
+    json.dump('!', output_file)
